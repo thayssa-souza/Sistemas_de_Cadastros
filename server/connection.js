c369e5df45ab1f4b734a0c2cb87df2acfde2e437
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-12-29 15:36:40 -0300

    Converting to tedious-promises

--- server/connection.js
+++ server/connection.js
@@ -1,88 +1,42 @@
-const Connection = require('tedious').Connection;
-const Request = require('tedious').Request;
+const tp = require('tedious-promises');
 const TYPES = require('tedious').TYPES;
 
 var connectionConfig = {
+    userName: 'user_trial',
+    password: '7412LIVE!@#$&*()',
     server: 'virtual2.febracorp.org.br',
-    authentication:{
-        type: 'default',
-        options:{
-            userName: 'user_trial',
-            password: '7412LIVE!@#$&*()'
-        }
-    },
     options:{
+        database: 'CONTOSO',
         encrypt: false,
-        database: 'CONTOSO'
     }
 };
 
 module.exports.insert = function insertRegistration(tableName, value, code){
-    var newConnection = new Connection(connectionConfig);
-    
-    newConnection.on('connect', error => {
-        if(error){
-            console.log("Não foi possível conectar-se ao banco. Tente novamente.", error)
-        } else{ 
-            const request = new Request(`INSERT INTO ${tableName} OUTPUT INSERTED.id VALUES (@value, @code)`, function(error){
-                if(error){
-                    console.log("Erro: ", error);
-                }
-            });
-            request.addParameter("value", TYPES.NVarChar, value);
-            request.addParameter("code", TYPES.BigInt, code);
-        
-            request.on("row", function(columns){
-                columns.forEach(function(column){
-                    if(column.value == null){
-                        console.log("Não é permitido campos vazios");
-                    } else {
-                        console.log("Cliente inserido com sucesso!");
-                    }
-                });
-            });
-            request.on("requestCompleted", function(rowCount, more){
-                newConnection.close();
-            });
-            newConnection.execSql(request);
-        }
-    });
-    newConnection.connect();
-}
+    tp.setConnectionConfig(connectionConfig);
+    tp.sql(`INSERT INTO ${tableName} VALUES ('${value}', ${code})`)
+    .returnRowCount()
+    .execute()
+    .then(function(rowCount){
+        console.log("Executado com sucesso!");
+    })
+    .fail(function(error){
+        console.log("Erro: ", error);
+    })
+};
 
 module.exports.selectCodName = function selectCodes(tableCode, code){
-    var newConnection = new Connection(connectionConfig);
-    
-    newConnection.on('connect', error => {
-        if(error){
-            console.log("Não foi possível conectar-se ao banco. Tente novamente.", error)
-        } else{ 
-            const requestSelect = new Request(`SELECT soma FROM ${tableCode} WHERE cod = @code`, function(error){
-                if(error){
-                    console.log("Erro: ", error);
-                }
-            });
-            requestSelect.addParameter("code", TYPES.BigInt, code);
-
-            requestSelect.on("row", function(columns){
-                columns.forEach(function(column){
-                    if(column.value == null){
-                        return console.log("Nulo.");
-                    } else {
-                        let codeSoma = parseInt(column.value) + parseInt(code);
-                        console.log(`${tableCode}: ${codeSoma}`);
-                        return codeSoma;
-                    }
-                });
-            });
-
-            requestSelect.on("requestCompleted", function(){
-                newConnection.close();
-                console.log("Terminou de rodar");
-            });
-            newConnection.execSql(requestSelect);
-        }
+    tp.setConnectionConfig(connectionConfig);
+    return tp.sql(`SELECT soma FROM ${tableCode} WHERE cod = '${code}'`)
+    .execute()
+    .then(function(result){
+        const sumResult = result[0].soma;
+        const somas = parseInt(sumResult) + parseInt(code);
+        console.log("Soma: ", sumResult);
+        console.log("Código: ", code);
+        console.log("Total: ", somas);
+        return somas;
+    })
+    .fail(function(error){
+        console.log("Erro: ", error);
     });
-    newConnection.connect();
 }
-
